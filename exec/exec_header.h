188f9a4ccc0851aa091f9e5656cb683d99722989
Ilyes Sedjal <ilsedjal@f6r11s9.clusters.42paris.fr>
2025-09-11 15:56:06 +0200

    header creation...

--- exec/exec_header.h
+++ exec/exec_header.h
@@ -1,12 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   error.c                                            :+:      :+:    :+:   */
+/*   exec_header.h                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ilsedjal <ilsedjal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/09/11 12:17:26 by ilsedjal          #+#    #+#             */
-/*   Updated: 2025/09/11 15:08:22 by ilsedjal         ###   ########.fr       */
+/*   Created: 2025/09/11 15:26:49 by ilsedjal          #+#    #+#             */
+/*   Updated: 2025/09/11 15:28:24 by ilsedjal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef MINISHELL_EXEC_H
+# define MINISHELL_EXEC_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <fcntl.h>
+# include <stdlib.h>
+# include <sys/wait.h>
+# include "Libft_42/libft.h"
+
+
+#endif
 
