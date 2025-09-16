135c38c9e36ad5166216525b82f6c11f978b3b85
Ilyes Sedjal <ilsedjal@f6r12s7.clusters.42paris.fr>
2025-09-16 15:10:14 +0200

    test final

--- exec/exec_header.h
+++ exec/exec_header.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exec_header.h                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fmoulin <fmoulin@student.42.fr>            +#+  +:+       +#+        */
+/*   By: ilsedjal <ilsedjal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/11 15:26:49 by ilsedjal          #+#    #+#             */
-/*   Updated: 2025/09/16 14:46:27 by fmoulin          ###   ########.fr       */
+/*   Updated: 2025/09/16 15:10:02 by ilsedjal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@
 # include <stdlib.h>
 # include <sys/wait.h>
 # include "../utils/libft/libft.h"
+
+// test final
 
 
 #endif
