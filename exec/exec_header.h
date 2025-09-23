b78c98131bd369cf338f26b7ff8d5720573f7389
Ilyes Sedjal <ilsedjal@f6r12s13.clusters.42paris.fr>
2025-09-23 14:43:25 +0200

    2 builtins, and exec single cmd

--- exec/exec_header.h
+++ exec/exec_header.h
@@ -6,7 +6,7 @@
 /*   By: ilsedjal <ilsedjal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/11 15:26:49 by ilsedjal          #+#    #+#             */
-/*   Updated: 2025/09/16 15:10:02 by ilsedjal         ###   ########.fr       */
+/*   Updated: 2025/09/23 14:33:41 by ilsedjal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,24 @@
 # include <stdlib.h>
 # include <sys/wait.h>
 # include "../utils/libft/libft.h"
+# include "../../minishell.h"
 
-// test final
 
+typedef struct s_shell
+{
+    char    *pwd;
+    char    *oldpwd;
+    char   **envp;         // ton tableau d’environnement
+    int      exit_status;
+}   t_shell;
 
+int		executre_cmds_list(t_cmd *cmds , t_shell *shell);
+int		check_n(const char *str);
+int		ft_echo(char **argv);
+int		ft_pwd(t_shell *shell);
+int		execute_cmds_list(t_cmd *cmds , t_shell *shell);
+int		exec_one_cmd(char **argv, char **envp);
+char	*find_path(char *args, char **envp);
+void	init_shell(t_shell *shell)
 #endif
 
