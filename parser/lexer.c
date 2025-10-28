b8db206c7843616e9da7b8d88435d7edb461c12b
C4r4cole <c4r4cole.chevalerie@gmail.com>
2025-10-28 14:16:50 +0100

    refactoring lexer.c

--- parser/lexer.c
+++ parser/lexer.c
@@ -6,7 +6,7 @@
 /*   By: fmoulin <fmoulin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/15 16:44:59 by fmoulin           #+#    #+#             */
-/*   Updated: 2025/10/27 18:32:51 by fmoulin          ###   ########.fr       */
+/*   Updated: 2025/10/28 14:15:23 by fmoulin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -191,6 +191,50 @@ int	expand_and_join(t_splitter *initialized, char *input, t_shell *shell)
 	return (0);
 }
 
+int	not_quoted_actions(t_splitter *initialized, char *input, t_shell *shell)
+{
+	if (add_split_meta_len(initialized, input))
+		return (1);
+	if (add_split_on_space(initialized, input))
+		return (1);
+	if (set_in_single(initialized, input))
+		return (1);
+	if (set_in_double(initialized, input))
+		return (1);
+	if (expand_and_join(initialized, input, shell))
+		return (1);
+	return (0);
+}
+
+int	double_quote_actions(t_splitter *initialized, char *input, t_shell *shell)
+{
+	if (unset_in_double(initialized, input))
+		return (1);
+	if (expand_and_join(initialized, input, shell))
+		return (1);
+	return (0);
+}
+
+int	actions_on_quotes(t_splitter *initialized, char *input, t_shell *shell)
+{
+	if (!initialized->in_single && !initialized->in_double)
+	{
+		if (not_quoted_actions(initialized, input, shell))
+			return (1);
+	}
+	else if (initialized->in_single)
+	{
+		if (unset_in_single(initialized, input))
+			return (1);
+	}
+	else if (initialized->in_double)
+	{
+		if(double_quote_actions(initialized, input, shell))
+			return (1);
+	}
+	return (0);
+}
+
 char **input_splitter(char *input, t_shell *shell)
 {
 	t_splitter *initialized;
@@ -198,38 +242,13 @@ char **input_splitter(char *input, t_shell *shell)
 	initialized = splitter_init();
     while (input[initialized->i])
 	{
-        if (!initialized->in_single && !initialized->in_double)
-		{
-			if (add_split_meta_len(initialized, input))
-				continue ;
-			if (add_split_on_space(initialized, input))
-				continue ;
-			if (set_in_single(initialized, input))
-				continue ;
-			if (set_in_double(initialized, input))
-				continue ;
-			if (expand_and_join(initialized, input, shell))
-				continue ;
-        }
-		else if (initialized->in_single)
-		{
-			if (unset_in_single(initialized, input))
-				continue ;
-        }
-		else if (initialized->in_double)
-		{
-			if (unset_in_double(initialized, input))
-				continue ;
-			if (expand_and_join(initialized, input, shell))
-				continue ;
-        }
+		if(actions_on_quotes(initialized, input, shell))
+			continue ;
         initialized->buf = str_append_char(initialized->buf, input[initialized->i]);
         initialized->i++;
     }
     if (initialized->buf[0] != '\0')
-	{
         initialized->output = add_split(initialized->output, &initialized->count, initialized->buf, ft_strlen(initialized->buf));
-    }
     free(initialized->buf);
     return (initialized->output);
 }
