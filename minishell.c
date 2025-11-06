6f713d2d54763121e3ccf0bfebaab02bdf9f2d78
C4r4cole <c4r4cole.chevalerie@gmail.com>
2025-11-06 20:37:09 +0100

    minishell done

--- minishell.c
+++ minishell.c
@@ -3,30 +3,62 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ilsedjal <ilsedjal@student.42.fr>          +#+  +:+       +#+        */
+/*   By: fmoulin <fmoulin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/11 14:49:27 by fmoulin           #+#    #+#             */
-/*   Updated: 2025/11/06 13:16:25 by ilsedjal         ###   ########.fr       */
+/*   Updated: 2025/11/06 20:35:56 by fmoulin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "exec/exec_header.h"
 #include "minishell.h"
 
-int	main(int argc, char **argv, char **envp)
+static int	main_init(t_shell **data, int argc, char **argv, char **envp)
+{
+	if (!*data)
+		return (1);
+	(void)argc;
+	(void)*argv;
+	init_shell(*data, envp);
+	rl_bind_key('\t', rl_complete);
+	using_history();
+	return (0);
+}
+
+static void	handle_sigint_status(t_shell *data)
+{
+	if (g_sig == SIGINT)
+	{
+		data->exit_status = 130;
+		g_sig = 0;
+	}
+}
+
+static void	process_input(char *input, t_shell *data)
 {
 	t_cmd	*tokens;
+
+	add_history(input);
+	tokens = parse_input(input, data);
+	free(input);
+	data->current_cmd_list = tokens;
+	if (tokens)
+	{
+		if (tokens->next)
+			execute_piped_cmds(tokens, data);
+		else
+			execute_cmds_list(tokens, data);
+		free_cmd_list(tokens);
+	}
+}
+
+int	main(int argc, char **argv, char **envp)
+{
 	t_shell	*data;
 	char	*input;
 
 	data = malloc(sizeof(t_shell));
-	if (!data)
-		return (1);
-	(void)argc;
-	(void)*argv;
-	init_shell(data, envp);
-	rl_bind_key('\t', rl_complete);
-	using_history();
+	main_init(&data, argc, argv, envp);
 	while (1)
 	{
 		setup_signals_main();
@@ -34,23 +66,8 @@ int	main(int argc, char **argv, char **envp)
 		input = readline("minishell> ");
 		if (!input)
 			break ;
-		if (g_sig == SIGINT)
-		{
-			data->exit_status = 130;
-			g_sig = 0;
-		}
-		add_history(input);
-		tokens = parse_input(input, data);
-		free(input);
-		data->current_cmd_list = tokens;
-		if (tokens)
-		{
-			if (tokens->next)
-				execute_piped_cmds(tokens, data);
-			else
-				execute_cmds_list(tokens, data);
-			free_cmd_list(tokens);
-		}
+		handle_sigint_status(data);
+		process_input(input, data);
 	}
 	free_shell(data);
 }
